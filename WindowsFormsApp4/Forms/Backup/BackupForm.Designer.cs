46b8c706aeff3746b298cc99544ff9ffad4de867
unknown <albhyrydhyab201@gmail.com>
2025-12-16 07:53:48 +0300

    initial

---
+++ WindowsFormsApp4/Forms/Backup/BackupForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp4.Forms.Backup
+{
+    partial class BackupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblLastBackup = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Back = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btb = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTitle.Location = new System.Drawing.Point(438, 67);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(287, 45);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "ROSTORE Part";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // lblLastBackup
+            // 
+            this.lblLastBackup.AutoSize = true;
+            this.lblLastBackup.BackColor = System.Drawing.Color.Transparent;
+            this.lblLastBackup.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastBackup.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblLastBackup.Location = new System.Drawing.Point(275, 174);
+            this.lblLastBackup.Name = "lblLastBackup";
+            this.lblLastBackup.Size = new System.Drawing.Size(229, 28);
+            this.lblLastBackup.TabIndex = 2;
+            this.lblLastBackup.Text = "Last Backup :None";
+            this.lblLastBackup.Click += new System.EventHandler(this.lblLastBackup_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(279, 260);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(400, 24);
+            this.txtPath.TabIndex = 3;
+            this.txtPath.Text = "Backup save location";
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnBrowse.ForeColor = System.Drawing.Color.White;
+            this.btnBrowse.Location = new System.Drawing.Point(721, 260);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(116, 30);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnBackup.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.data_recovery__3_;
+            this.btnBackup.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnBackup.ForeColor = System.Drawing.Color.White;
+            this.btnBackup.Location = new System.Drawing.Point(262, 310);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(200, 137);
+            this.btnBackup.TabIndex = 5;
+            this.btnBackup.Text = "Create backup";
+            this.btnBackup.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBackup.UseVisualStyleBackColor = false;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnRestore.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.button_reload_15002;
+            this.btnRestore.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnRestore.ForeColor = System.Drawing.Color.White;
+            this.btnRestore.Location = new System.Drawing.Point(499, 310);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(200, 137);
+            this.btnRestore.TabIndex = 6;
+            this.btnRestore.Text = "Restore Bockup";
+            this.btnRestore.UseVisualStyleBackColor = false;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.BackgroundImage = global::WindowsFormsApp4.Properties.Resources._0bb87d92_4971_4d57_9d5a_2b0e6761725b1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btb);
+            this.panel1.Controls.Add(this.btn_Back);
+            this.panel1.Controls.Add(this.btn_Close);
+            this.panel1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1188, 43);
+            this.panel1.TabIndex = 7;
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Back.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.IMG_٢٠٢٥١١٣٠_١٧٥٠١٧_٠٥٥١٤٨_2_;
+            this.btn_Back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Back.Location = new System.Drawing.Point(1070, 3);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(25, 25);
+            this.btn_Back.TabIndex = 9;
+            this.btn_Back.UseVisualStyleBackColor = true;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Close.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.close;
+            this.btn_Close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Close.Location = new System.Drawing.Point(1154, 3);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(25, 25);
+            this.btn_Close.TabIndex = 8;
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(274, 509);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(544, 28);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Dental Clinic Management System  Versin 1.0";
+            // 
+            // btb
+            // 
+            this.btb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btb.BackColor = System.Drawing.Color.Transparent;
+            this.btb.ForeColor = System.Drawing.Color.Transparent;
+            this.btb.Location = new System.Drawing.Point(1112, 3);
+            this.btb.Name = "btb";
+            this.btb.Size = new System.Drawing.Size(25, 25);
+            this.btb.TabIndex = 20;
+            this.btb.UseVisualStyleBackColor = false;
+            this.btb.Click += new System.EventHandler(this.btb_Click);
+            // 
+            // BackupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp4.Properties.Resources._0f87309e_8bb3_4368_aaf1_00883da57012;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1188, 653);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblLastBackup);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "BackupForm";
+            this.Text = "BackupForm";
+            this.Load += new System.EventHandler(this.BackupForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblLastBackup;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Back;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btb;
+    }
+}
---
+++ WindowsFormsApp4/Forms/Backup/BackupForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using WindowsFormsApp4.Forms.Dashboard;
+
+namespace WindowsFormsApp4.Forms.Backup
+{
+    public partial class BackupForm : Form
+    {
+        private string connectionString = "Server=.\\SQLEXPRESS;Database=ClinicDB;Trusted_Connection=True;";
+        private string backupFolder = @"D:\Backup\";
+        public BackupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BackupForm_Load(object sender, EventArgs e)
+        {
+          
+            lblLastBackup.Text = "Last Backup: لا يوجد";
+            if (!Directory.Exists(backupFolder))
+            {
+                Directory.CreateDirectory(backupFolder);
+            }
+        }
+        
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLastBackup_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+
+        
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"D:\Backup";       
+            ofd.Filter = "Backup Files (*.bak)|*.bak";  
+            ofd.Title = "اختر ملف النسخة الاحتياطية";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = ofd.FileName;           
+            }
+        
+           
+        }
+
+        
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+         
+            if (string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                MessageBox.Show("اختر ملف النسخة الاحتياطية أولاً");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string query = $@"
+            ALTER DATABASE ClinicDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+            RESTORE DATABASE ClinicDB 
+            FROM DISK = '{txtPath.Text}' 
+            WITH REPLACE;
+
+            ALTER DATABASE ClinicDB SET MULTI_USER;
+            ";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("تم استعادة النسخة الاحتياطية بنجاح ");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطأ: " + ex.Message);
+            }
+        
+        
+        }
+
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string backupName = $"ClinicDB_{DateTime.Now:yyyyMMdd_HHmm}.bak";
+                string fullPath = Path.Combine(backupFolder, backupName);
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "BACKUP DATABASE ClinicDB TO DISK = @path";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@path", fullPath);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+                lblLastBackup.Text = "Last Backup: " + DateTime.Now.ToString();
+                txtPath.Text = fullPath;
+
+                MessageBox.Show(" تم إنشاء النسخة الاحتياطية بنجاح في D:\\Backup\\");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطأ: " + ex.Message);
+            }
+
+        }
+
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            Form d = new DashboardForm();
+            d.ShowDialog();
+            this.Close();
+        }
+
+        private void btb_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+
+                this.WindowState = FormWindowState.Maximized;
+
+            else this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
